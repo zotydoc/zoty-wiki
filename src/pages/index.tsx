77288d3c3afa89d3c89bd4fd5c420b3c408aa585
Robert Vajdík <robert.vajdik@idsa.cz>
2024-10-24 21:12:31 +0200

    doc/manual to docs/manual

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Zoty tutoriál - 5min ⏱️
+            to="/docs/manual">
+            Zoty uživatelský manuál - 30min ⏱️
           </Link>
         </div>
       </div>
